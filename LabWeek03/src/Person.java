045c8d5d8d965e8808e679a33b285793f197153d
yearagen <yearagen@dh2020pc07.utm.utoronto.ca>
2017-09-18 15:59:00 -0400

    These are my Person and SodaCan classes.
    They are currently incomplete and will be completed later in the day.

--- LabWeek03/src/Person.java
+++ LabWeek03/src/Person.java
@@ -1,4 +1,25 @@
 public class Person {
+	
+	private String thirstStatus;
+	private int amountDrunk;
+	
+	public Person(String thirstStatus, int amountDrunk) {
+		this.amountDrunk = amountDrunk;
+		this.thirstStatus = "very thirsty";
+		
+
+	public void sipFrom(Object sodacan) {
+		amountDrunk += sodacan.sip();
+	}
+	
+	public void gulpFrom(Object sodacan) {
+		amountDrunk += sodacan.gulp();
+	}
+	
+	public String getThirstStatus() {
+		return this.thirstStatus;
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- LabWeek03/src/SodaCan.java
+++ LabWeek03/src/SodaCan.java
@@ -1,8 +1,55 @@
 public class SodaCan {
+	
+	private String type;
+	private int amount;
+	private boolean isOpen;
+	
+	public SodaCan(String type, boolean isOpen, int amount) {
+		this.type = type;
+		this.amount = 250;
+		this.isOpen = isOpen;
+	}
+	
+	public void open() {
+		if !(opened){
+			this.opened = true;
+		}
+	}
+	public static int sip() {
+		if (this.isOpen && this.amount >= 10) {
+			this.amount -= 10;
+			return 10;
+		}
+		else {
+			int remainder = this.amount;
+			this.amount = 0;
+			return remainder
+		}
+	}
+	
+	public static int gulp() {
+		if (this.isOpen && this.amount >= 50){
+			this.amount -= 50;
+			return 50;
+		}
+		else {
+			int remainder = this.amount;
+			this.amount = 0;
+			return remainder;
+		}
+	}
+	
+	public String toString() {
+		return "Soda type: " + this.type + ", Opened: " + this.isOpen + ", amount: " + this.amount;
+	}
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SodaCan can1 = new SodaCan("Sprite", false);
+		SodaCan can2 = new SodaCan("Root Beer", true);
+		SodaCan can3 = new SodaCan("Coke", true);
+		can3.sip();
+		System.out.println(can3.amount);
 	}
 
 }
