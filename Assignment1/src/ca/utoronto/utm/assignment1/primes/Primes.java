ed0c224ce46ef834f3db9600b2b48c9223fb3bcc
Alexander <Alexander@192.168.2.252>
2017-09-24 20:46:08 -0400

    Assignment 1: Non-final Commit of Part 1
    The following changes were made:
    -Added the 4 remaining methods for class.
    -Added JUnit test cases for all the methods.
    -Added Javadocs for both Prime.java and its test case file.
    
    TO-DO:
    -add more JUnit test cases
    -Might change javadoc to indicate performance for each method
    -Might change methods

--- Assignment1/src/ca/utoronto/utm/assignment1/primes/Primes.java
+++ Assignment1/src/ca/utoronto/utm/assignment1/primes/Primes.java
@@ -1,6 +1,12 @@
 package ca.utoronto.utm.assignment1.primes;
 import java.util.ArrayList;
+import java.util.Arrays;
 
+/**
+ * @author Alexander
+ * 
+ * This class defines different methods regarding primes.
+ */
 public class Primes {
     public static void main(String [] args){
             System.out.println(primes(10000)[9999]);
@@ -10,7 +16,7 @@ public class Primes {
             ArrayList<Integer> a;
             long start, end;
             
-            /**
+            
             start = System.currentTimeMillis();
             a = primesLessThan(n);
             end = System.currentTimeMillis();
@@ -29,9 +35,14 @@ public class Primes {
             start = System.currentTimeMillis();
             a = primesLessThanSieveRemove2(n);
             end = System.currentTimeMillis();
-            System.out.println(a.size()+ " " + (end-start));
-            **/
+            System.out.println(a.size()+ " " + (end-start));          
     }
+	/**
+	 * This method determines whether a given non-negative integer n is a prime number.
+	 * 
+	 * @param n Non-negative integer to which this method determines if it is prime or not.
+	 * @return Returns true if the given integer n is prime. Returns false if otherwise.
+	 */
 	public static boolean isPrime(int n) {
 		if (n < 2) {
 			return false;
@@ -45,6 +56,12 @@ public class Primes {
 		}
 	}
 
+	/**
+	 * This method returns an array of the first n primes.
+	 * 
+	 * @param n Amount of the first primes to be return within an array.
+	 * @return Returns an array of the first n primes.
+	 */
 	public static int[] primes(int n) {
 		int current = 2;
 		int num_primes = 0;
@@ -59,4 +76,96 @@ public class Primes {
 		}
 		return list_of_primes;
 	}
+	
+	/**
+	 * This method returns an array of primes that are less than a given non-negative integer n.
+	 * 
+	 * @param n Maximum integer by which an array of primes less than it will be formed.
+	 * @return Returns an ArrayList of all the prime numbers that are less than n.
+	 */
+	
+	public static ArrayList<Integer> primesLessThan(int n) {
+		ArrayList<Integer> array_of_primes = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			if (isPrime(i)){
+				array_of_primes.add(i);
+			}		
+		}
+		return array_of_primes;
+	}
+	
+	/**
+	 * This method returns an array of primes that are less than a given non-negative integer n.
+	 * 
+	 * @param n Maximum integer by which an array of primes less than it will be formed.
+	 * @return Returns an ArrayList of all the prime numbers that are less than n.
+	 */
+	public static ArrayList<Integer> primesLessThanSieveRemove(int n) {
+		ArrayList<Integer> primes = new ArrayList<>();		
+		for (int i = 2; i < n; i++) {
+			primes.add(i);
+		}
+		ArrayList<Integer> referenceList = new ArrayList<>(primes);
+		for (int number: referenceList) {
+			for (int other_number:referenceList.subList(referenceList.indexOf(number) + 1, referenceList.size())){
+				if (other_number % number == 0) {
+					primes.remove((Object)other_number);
+				}
+			}
+		}
+		return primes;
+	}
+	
+	/**
+	 * This method returns an array of primes that are less than a given non-negative integer n.
+	 * 
+	 * @param n Maximum integer by which an array of primes less than it will be formed.
+	 * @return Returns an ArrayList of all the prime numbers that are less than n.
+	 */
+	public static ArrayList<Integer> primesLessThanSieveAdd(int n){
+		ArrayList<Integer> primes = new ArrayList<>();
+		for (int i = 2; i < n; i++) {
+			boolean dividesBy = false;
+			for (int prime: primes) {
+				if (i % prime == 0) {
+					dividesBy = true;
+				}
+			}
+			if (dividesBy == false) {
+				primes.add(i);
+			}
+		}
+		return primes;
+	}
+	
+	/**
+	 * This method returns an array of primes that are less than a given non-negative integer n.
+	 * 
+	 * @param n Maximum integer by which an array of primes less than it will be formed.
+	 * @return Returns an ArrayList of all the prime numbers that are less than n.
+	 */
+	public static ArrayList<Integer> primesLessThanSieveRemove2(int n){
+		boolean[] primes = new boolean[n];
+		Arrays.fill(primes, true);
+		for (int i = 2; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (j % i == 0 && j != i) {
+					primes[j] = false;
+				}
+			}
+		}
+		ArrayList<Integer> finalPrimes = new ArrayList<>();
+		for (int x = 2; x < n; x++) {
+			if (primes[x] == true) {
+				finalPrimes.add(x);
+			}
+		}
+		return finalPrimes;
+	}
 }
+
+
+
+/* Answer to point g.
+ * 
+ * The method primeLessThan is fastest than the other primeLessThan* methods because */
---
+++ Assignment1/src/ca/utoronto/utm/assignment1/primes/primesLessThanTest.java
@@ -0,0 +1,169 @@
+package ca.utoronto.utm.assignment1.primes;
+import java.util.ArrayList;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * @author Alexander
+ *
+ * This class defines JUnit test cases for all the methods in Primes.java. It also defines different variables for ArrayLists, booleans and in[] arrays for these test cases.
+ */
+public class primesLessThanTest {
+	ArrayList<Integer> a;
+	ArrayList<Integer> b;
+	ArrayList<Integer> c;
+	ArrayList<Integer> d;
+	ArrayList<Integer> e;
+	ArrayList<Integer> f;
+	boolean num1;
+	boolean num2;
+	boolean num3;
+	boolean num4;
+	boolean num5;
+	boolean num6;
+	int[] array1;
+	int[] array2;
+	int[] array3;
+	int[] array4;
+	
+	
+	
+	/**
+	 * Test cases for the IsPrime() method from Primes.java
+	 */
+	@Test
+	public void testIsPrime() {
+		num1 = Primes.isPrime(0);
+		num2 = Primes.isPrime(1);
+		num3 = Primes.isPrime(2);
+		num4 = Primes.isPrime(47);
+		num5 = Primes.isPrime(997);
+		num6 = Primes.isPrime(1000);
+		// assert statements
+		 assertEquals(false, num1);
+		 assertEquals(false, num2);
+		 assertEquals(true, num3);
+		 assertEquals(true, num4);
+		 assertEquals(true, num5);
+		 assertEquals(false, num6);
+	}
+
+	/**
+	 * Test cases for the Primes() method from Primes.java.
+	 */
+	@Test
+	public void testPrimes() {
+		array1 = Primes.primes(10);
+		array2 = Primes.primes(1);
+		array3 = Primes.primes(1000);
+		array4 = Primes.primes(0);
+		// assert statements
+		assertEquals(10, array1.length);
+		 assertEquals(1, array2.length);
+		 assertEquals(1000, array3.length);
+		 assertEquals(0, array4.length);
+		 assertTrue(array1[1] == 3);
+		 assertTrue(array1[array1.length - 1] == 29);
+		 
+	}
+	
+	/**
+	 * Test cases for the primesLessThan() method from Primes.java.
+	 */
+	@Test
+	public void testPrimesLessThan() {
+		 a = Primes.primesLessThan(10);
+		 b = Primes.primesLessThan(0);
+		 c = Primes.primesLessThan(51);
+		 d = Primes.primesLessThan(2);
+		 e = Primes.primesLessThan(4);
+		 f = Primes.primesLessThan(1000);
+		 // assert statements
+		 assertEquals(4, a.size());
+		 assertEquals(0, b.size());
+		 assertEquals(15, c.size());
+		 assertEquals(0, d.size());
+		 assertEquals(2, e.size());
+		 assertTrue(e.get(0) == 2);
+		 assertTrue(a.get(3) == 7);
+		 assertTrue(c.get(14) == 47);
+		 assertEquals(168, f.size());
+		 assertTrue(f.get(167) == 997);
+		 
+		 
+		 
+	}
+
+	/**
+	 * Test cases for the primesLessThanSieveRemove() method from Primes.java
+	 */
+	@Test
+	public void testPrimesLessThanSieveRemove() {
+		 a = Primes.primesLessThanSieveRemove(10);
+		 b = Primes.primesLessThanSieveRemove(0);
+		 c = Primes.primesLessThanSieveRemove(51);
+		 d = Primes.primesLessThanSieveRemove(2);
+		 e = Primes.primesLessThanSieveRemove(4);
+		 f = Primes.primesLessThanSieveRemove(1000);
+		 // assert statements
+		 assertEquals(4, a.size());
+		 assertEquals(0, b.size());
+		 assertEquals(15, c.size());
+		 assertEquals(0, d.size());
+		 assertEquals(2, e.size());
+		 assertTrue(e.get(0) == 2);
+		 assertTrue(a.get(3) == 7);
+		 assertTrue(c.get(14) == 47);
+		 assertEquals(168, f.size());
+		 assertTrue(f.get(167) == 997);
+	}
+
+	/**
+	 * Test cases for the primesLessThanSieveAdd() method from Primes.java
+	 */
+	@Test
+	public void testPrimesLessThanSieveAdd() {
+		 a = Primes.primesLessThanSieveAdd(10);
+		 b = Primes.primesLessThanSieveAdd(0);
+		 c = Primes.primesLessThanSieveAdd(51);
+		 d = Primes.primesLessThanSieveAdd(2);
+		 e = Primes.primesLessThanSieveAdd(4);
+		 f = Primes.primesLessThanSieveAdd(1000);
+		 // assert statements
+		 assertEquals(4, a.size());
+		 assertEquals(0, b.size());
+		 assertEquals(15, c.size());
+		 assertEquals(0, d.size());
+		 assertEquals(2, e.size());
+		 assertTrue(e.get(0) == 2);
+		 assertTrue(a.get(3) == 7);
+		 assertTrue(c.get(14) == 47);
+		 assertEquals(168, f.size());
+		 assertTrue(f.get(167) == 997);
+	}
+
+	/**
+	 * Test cases for the primesLessThanSieveRemove2() method from Primes.java
+	 */
+	@Test
+	public void testPrimesLessThanSieveRemove2() {
+		 a = Primes.primesLessThanSieveRemove2(10);
+		 b = Primes.primesLessThanSieveRemove2(0);
+		 c = Primes.primesLessThanSieveRemove2(51);
+		 d = Primes.primesLessThanSieveRemove2(2);
+		 e = Primes.primesLessThanSieveRemove2(4);
+		 f = Primes.primesLessThanSieveRemove2(1000);
+		 // assert statements
+		 assertEquals(4, a.size());
+		 assertEquals(0, b.size());
+		 assertEquals(15, c.size());
+		 assertEquals(0, d.size());
+		 assertEquals(2, e.size());
+		 assertTrue(e.get(0) == 2);
+		 assertTrue(a.get(3) == 7);
+		 assertTrue(c.get(14) == 47);
+		 assertEquals(168, f.size());
+		 assertTrue(f.get(167) == 997);
+	}
+
+}
